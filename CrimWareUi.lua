49da58e63ace1f6d6873f41b9095f2289b5c5eea
21xDecay <128763846+21xDecay@users.noreply.github.com>
2024-09-08 21:03:39 -0400

    Update CrimWareUi.lua

--- CrimWareUi.lua
+++ CrimWareUi.lua
@@ -571,7 +571,7 @@ function library:InitNotifications(text, duration, callback)
         elseif type == "success" then
             bar.BackgroundColor3 = Color3.fromRGB(131, 255, 103)
         elseif type == "information" then
-            bar.BackgroundColor3 = Color3.fromRGB(126, 117, 255)
+            bar.BackgroundColor3 = Color3.fromRGB(181, 220, 249)
         end
     
         barCorner.CornerRadius = UDim.new(0, 2)
